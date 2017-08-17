176fccfdbf1dbd484c1085bf0fc84c5125bce360
bsk <bartlomiejglowala@gmail.com>
2017-08-17 09:24:35 +0200

    async iterator util. ground generation testing

--- src/sprites/Ground.js
+++ src/sprites/Ground.js
@@ -1,25 +1,89 @@
 import Phaser from 'phaser'
 import { getCurvePoints } from 'cardinal-spline-js'
 import spline from 'cubic-spline'
+import { AsyncIterator } from '../utils';
+
+  // const async = new AsyncIterator([1,2,3,4,5], console.log.bind(null, Date.now()), 1);
+  // async.process().then(()=>console.log('yay!'));
+
+const cubicSpline = (points) => {
+  const values = {
+    xs: [],
+    ys: []
+  }
+  points.forEach((pt, i) => {
+    const arr = !(i%2) ? 'xs' : 'ys';
+    values[arr].push(pt);
+  });
+
+  console.log(values);
+
+  const newArr = [];
+  for(let i = values['xs'][0]; i < values['xs'][values['xs'].length - 1]; i++) {
+    newArr.push(i, spline(i, values.xs, values.ys))
+  }
+  return newArr;
+}
 
 export default class Ground {
   constructor (game) {
     this.game = game
-    console.log(game.antialias)
-    this.points = [0, this.game.height, 50, 100, 175, 200, 50, 250, this.game.width, this.game.height]
-    this.polyMock = new Phaser.Polygon(...getCurvePoints(this.points).map(pt => parseInt(pt)))
-    // this.graphics = game.add.graphics(0, 0)
-    this.bmd = game.add.bitmapData(this.game.width, this.game.height)
-    this.graphics = this.bmd.context
-    this.bmd.addToWorld()
-    console.log(this.bmd, this.graphics)
-    // this.graphics.lineStyle(10, 0xFF0000, 0.8)
-    // this.graphics.beginFill(0xFF33ff)
-    // this.graphics.drawPolygon(this.poly.points)
-    // this.graphics.endFill()
+    this.offset = 0;
 
+    this.points = []
+    for (let i = -100; i < this.game.width + 3000; i+=100) {
+      this.points.push(i, parseInt(this.game.height - (Math.random() * 100 + 20), 10))
+    }
+
+    this.bmd = game.add.bitmapData(this.game.width, this.game.height)
+    this.ctx = this.bmd.context
+    const ctx = this.ctx;
+    // this.bmd.addToWorld()
     game.add.image(0, 0, this.bmd).anchor.set(0, 0)
-    window.spline = spline;
+
+
+
+
+    // this.poly = cubicSpline(this.points);
+    this.poly = getCurvePoints(this.points);
+    console.log(this.poly)
+    this.renderGround(this.poly.slice(0, (this.game.width + 105) * 2));
+    this.generateGround = this.generateGround.bind(this);
+    setTimeout(this.generateGround);
+
+  }
+
+  generateGround() {
+      if (this.poly.length > 2000) {
+        setTimeout(this.generateGround, 10);
+        return;
+      }
+      console.log('generateGround');
+      this.points = this.points.slice(-10);
+      const length = this.points[this.points.length-2];
+      for (let i = this.points[this.points.length-2]+100; i < length + 1500; i+=100) {
+        this.points.push(i, parseInt(this.game.height - (Math.random() * 100 + 20), 10))
+      }
+      this.points = this.points.slice(8, this.points.length)
+      console.log( this.points);
+      this.poly = [...this.poly, ...getCurvePoints(this.points)];
+
+      setTimeout(this.generateGround, 10);
+  }
+
+  renderGround (points, offset = 0) {
+    const ctx = this.ctx;
+    ctx.clearRect(0, 0, this.bmd.canvas.width, this.bmd.canvas.height);
+    ctx.beginPath()
+    ctx.moveTo(points[0]- offset, points[1])
+    for (var item = 2; item < points.length - 1; item += 2) {
+      // console.log(points[item], points[item + 1])
+      ctx.lineTo(points[item] - offset, points[item + 1])
+    }
+    // ctx.closePath()
+    // ctx.fill()
+    ctx.lineWidth = 10
+    ctx.stroke()
   }
 
   checkCollision ({x, y}) {
@@ -27,85 +91,12 @@ export default class Ground {
   }
 
   update ({x, y}) {
-    // console.log('points', getCurvePoints(this.points));
-    // this.angle += 1
-
-    // this.graphics.clear()
-    // this.bmd = this.game.add.bitmapData(this.game.world.width,
-    //   this.game.world.height)
-    this.graphics.clearRect(0, 0, this.bmd.canvas.width, this.bmd.canvas.height)
-    const oldPoints = this.points
-    delete this.points
-    this.points = oldPoints.map((pt, i) => i % 2 ? pt : pt - 5)
-    this.points = this.points.slice(2).slice(0, this.points.length - 4)
-    if (this.points[2] < -200) {
-      this.points = this.points.slice(2)
+    this.offset += 10;
+    if (this.poly[0] < this.offset - 20) {
+      // console.log('cut poly')
+      this.poly = this.poly.slice(-(this.poly.length-4))
+      // console.log(this.poly);
     }
-    if (this.points[this.points.length - 2] < this.game.width + 100) {
-      this.points.push(this.game.width + 200, parseInt(this.game.height - (Math.random() * 100 + 20), 10))
-    }
-    // console.log(this.points);
-    this.cubicPoints = this.points.reduce((res, cur) => {
-      if (!((res[0].length + res[1].length) % 2)) {
-        return [[...res[0], cur], res[1]]
-      } else {
-        return [res[0], [...res[1], cur]]
-      }
-    }, [[], []])
-    const newArr = [];
-    const diff = (Math.abs(this.cubicPoints[0][0] - this.cubicPoints[0][this.cubicPoints[0].length - 1]));
-    for(let i = this.cubicPoints[0][0]; i < this.cubicPoints[0][this.cubicPoints[0].length - 1]; i++) {
-      newArr.push(i, spline(i*.1, this.cubicPoints[0], this.cubicPoints[1]))
-    }
-    // console.log(newArr);
-
-    this.points = [-200, this.game.height + 20, ...this.points, this.game.width + 200, this.game.height + 20]
-    this.poly = newArr
-    this.polyMock = new Phaser.Polygon(...this.poly)
-
-    if (this.polyMock.contains(x, y)) {
-      this.graphics.fillStyle = '#f00'
-    } else {
-      this.graphics.fillStyle = '#B4D455'
-    }
-
-    [-235, -130, -25, 80, 185, 290, 395, 500, 605, 710, 815, 920]
-
-
-    this.graphics.beginPath()
-    this.graphics.moveTo(this.poly[0], this.poly[1])
-    for (var item = 2; item < this.poly.length - 1; item += 2) {
-      // console.log(this.poly[item], this.poly[item + 1])
-      this.graphics.lineTo(this.poly[item] + 0.5, this.poly[item + 1] + 0.5)
-    }
-
-    this.graphics.closePath()
-    this.graphics.fill()
-
-    this.graphics.lineWidth = 10
-    this.graphics.stroke()
-
-    // this.bmd.addToWorld()
-
-    // this.graphics.lineStyle(10, 0x000000)
-    // this.graphics.fillStyle = this.graphics.createPattern(this.cache.getImage('fill1'), 'repeat');
-    // if (this.poly.contains(x, y)) {
-    //   this.graphics.beginFill(0xFF3300)
-    // } else {
-    //   this.graphics.beginFill(0xB4D455)
-    // }
-
-    // this.graphics.drawPolygon(this.poly.points)
-    // this.graphics.endFill()
-    for (var i = 0; i < this.points.length; i++) {
-      if (!(i % 2)) {
-        this.graphics.fillRect(this.points[i] - 2, this.points[i + 1] - 2, 5, 5)
-      }
-    }
-    for (i = 0; i < this.poly.length; i++) {
-      if (!(i % 2)) {
-        this.graphics.fillRect(this.poly[i] - 1, this.poly[i + 1] - 1, 1, 10)
-      }
-    }
+    this.renderGround(this.poly.slice(0, (this.game.width + 105) * 2), this.offset)
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,72 @@ export const centerGameObjects = (objects) => {
     object.anchor.setTo(0.5)
   })
 }
+
+/**
+ * Batch processes large datasets with the help of setInterval in order
+ * to not lock up the single thread.
+ */
+
+	/**
+	 * Runs a callback on a large dataset without holding up the main thread.
+	 *
+	 * @param {array}		arr 		The dataset
+	 * @param {function}	cb 			The callback function
+	 * @param {int}			batchSize	(Optional) the number of operations per call, default: 10
+	 */
+	export const AsyncIterator = function(arr, cb, batchSize) {
+		this.arr = arr;
+		this.cb = cb;
+		this.batchSize = parseInt(batchSize) || 10;
+		this.position = 0;
+		this.results = [];
+
+		this.timer = null;
+
+		this._deferred;
+	}
+
+	/**
+	 * Starts the iteration of the dataset
+	 *
+	 * @return {Jquery deferred}	A promise that will resolve when operations are done.
+	 */
+	AsyncIterator.prototype.process = function() {
+		this._deferred = new Promise((resolve) => {
+      this.resolve = resolve;
+      this.timer = setInterval(this._iterate.bind(this), 10);
+    });
+
+		return this._deferred;
+	}
+
+	/**
+	 * The actual operating part
+	 * @private
+	 */
+	AsyncIterator.prototype._iterate = function() {
+		for(var i = 0; i  < this.batchSize; i++) {
+			if(this.position >= this.arr.length) { break; }
+
+			var currentItem = this.arr[this.position];
+
+			this.results[this.position] = this.cb(currentItem, this.position);
+
+			this.position++;
+		}
+
+		if(this.position >= this.arr.length) {
+            clearInterval(this.timer)
+			this.resolve(this.results);
+		}
+	}
+
+	/**
+	 * The public method for creating the AsyncIterator
+	 *
+	 * @return {Jquery deferred}	A promise that will resolve when operations are done.
+	//  */
+	// return function(arr, cb, size) {
+	// 	var it = new AsyncIterator(arr, cb, size);
+	// 	return it.process();
+	// }
